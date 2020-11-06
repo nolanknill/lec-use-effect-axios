5345a9a8b270b4b14ffbdd382ee7315d526218cd
Michael <mdismatsek@brainstation.io>
2020-11-06 09:50:40 -0500

    Change redirect to not be diving deeper

--- src/components/pages/ProductPage.js
+++ src/components/pages/ProductPage.js
@@ -4,7 +4,7 @@ import { Redirect } from "react-router-dom";
 function ProductPage(props) {
     const product = products.find((product) => product.id === props.match.params.productId);
 
-    // Diving deeper: redirect to Products page if no product found instead of erroring
+    // Redirect to Products page if no product found instead of erroring
     if (!product) return <Redirect to="/products" />;
 
     return (
