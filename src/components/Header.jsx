af5e7cc3c55dc9225e21f89bdf7a48676dca30a5
Nolan Knill <nolanknill@gmail.com>
2023-05-23 13:41:15 -0400

    Adds sass package, adds styles for active nav links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,39 @@
 import { NavLink } from "react-router-dom";
+import "./Header.scss";
 
 function Header() {
+    const navLinkActive = ({ isActive }) => {
+        if (isActive) {
+            return "header__link header__link--active";
+        } else {
+            return "header__link";
+        }
+    }
+
     return (
-        <header>
+        <header className="header">
             <h1>E-Commerce Site</h1>
             <nav>
                 <ul>
                     <li>
-                        <NavLink to="/">Home</NavLink>
+                        <NavLink className={navLinkActive} to="/">
+                            Home
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/contact">Contact</NavLink>
+                        <NavLink className={navLinkActive} to="/contact"> 
+                            Contact
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/products">Products</NavLink>
+                        <NavLink className={navLinkActive} to="/products">
+                            Products
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
         </header>
-    );
+    )
 }
 
 export default Header;
